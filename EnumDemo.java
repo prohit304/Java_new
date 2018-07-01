8c2d52f3591d77b579745c53af57ad2957145d74
P Rohit <40255346+prohit304@users.noreply.github.com>
2018-07-01 16:35:38 +0530

    Created enum example

---
+++ EnumDemo.java
@@ -0,0 +1,11 @@
+public class EnumDemo
+{
+enum Week{
+MONDAY,TUESDAY,WEDNESDAY,THURSDAU,FRIDAY,SATURDAY,SUNDAY
+    }
+public static void main(String args[]){
+Week weekDay;
+weekDay=Week.day;
+System.out.println("Value of WeekDay :"+weekDay);
+    }
+}
