fd9982e7996c8d081343c0660d333af2c099e579
P Rohit <40255346+prohit304@users.noreply.github.com>
2018-07-06 08:25:48 +0530

    Updated

--- String_demo.java
+++ String_demo.java
@@ -15,12 +15,12 @@ public class String_demo {
 		String st6=new String (a,0,7);
 		String st7=new String ();
 		//String st8; CTE
-		System.out.println("St1"+st1);
-		System.out.println("St2"+st2);
-		System.out.println("St3"+st3);
-		System.out.println("St4"+st4);
-		System.out.println("St5"+st5);
-		System.out.println("St6"+st6);
+		System.out.println("St1 :"+st1);
+		System.out.println("St2 :"+st2);
+		System.out.println("St3 :"+st3);
+		System.out.println("St4 :"+st4);
+		System.out.println("St5 :"+st5);
+		System.out.println("St6 :"+st6);
 		System.out.println("St7"+st7);
 		//String st8 cant be called Its CTE
 	}
