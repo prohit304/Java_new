4975884abb19dd5c7e6973977b2f1055c57a8176
P Rohit <40255346+prohit304@users.noreply.github.com>
2018-08-10 08:56:55 +0530

    StringBuffer Examples

---
+++ StringBuffer.java
@@ -0,0 +1,20 @@
+package Java_new;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+ class StringBuffer {
+
+	public static void main(String[] args) throws IOException {
+		
+		StringBuffer sb=new StringBuffer();
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter Surname");
+		String sur=br.readLine();
+		
+
+	}
+
+}
